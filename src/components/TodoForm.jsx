9cf34a7f319e99f7b1483cc2d0e9f2eda5d388b9
Andrey <Andreyadad@gmail.com>
2024-05-05 20:54:13 +0500

    Working on the Form заменил предыдущий вывод в консоль на setValue затем добавили в handleSubmit e.preventDefault() и вывели в консоль value Теперь если набрать в поле что то и нажать Enter то мы в консоли увидим эту набранную строку

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,9 +2,15 @@ import React, {useState} from 'react'
 
 export const TodoForm = () => {
     const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(value)
+    }
   return (
-    <form className='TodoForm'>
-        <input type='text' className='todo-input' placeholder='What is the task today?' onChange={(e) => console.log(e.target.value)} />
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)} />
         <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
