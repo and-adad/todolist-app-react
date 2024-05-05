13428a61b4a10ea645061c4459f3cdc9d1929c67
Andrey <Andreyadad@gmail.com>
2024-05-05 20:41:11 +0500

    Working on the Form добавил название кнопки и она стала прямоугольной

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -4,7 +4,7 @@ export const TodoForm = () => {
   return (
     <form className='TodoForm'>
         <input type='text' className='todo-input' placeholder='What is the task today?' />
-        <button type='submit' className='todo-btn'></button>
+        <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
 }
