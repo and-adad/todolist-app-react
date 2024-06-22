e7ffce6f379f757f94780e27f5e70995518f9f56
Andrey <Andreyadad@gmail.com>
2024-06-22 20:05:20 +0500

    Скопировал из TodoForm в EditTodoForm всё

--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import {useState} from 'react'
 
-export const EditTodoForm = () => {
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        addTodo(value)
+    }
   return (
-    <div>EditTodoForm</div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)} />
+        <button type='submit' className='todo-btn'>Add Task</button>
+    </form>
   )
 }
