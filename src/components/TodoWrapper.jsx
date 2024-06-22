3d0f766487c84761c3213596e1ddbec990389221
Andrey <Andreyadad@gmail.com>
2024-06-22 20:03:04 +0500

    По клике на иконку удаления задача удаляется, а при клике на редактирование далее доделаем

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -17,12 +17,20 @@ export const TodoWrapper = () => {
       setTodos(todos.map(todo => todo.id === id ? {...
         todo, completed: !todo.completed} : todo))
     }
+
+    const deleteTodo = id => {
+      setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    const editTodo = id => {
+      setTodos(todos.map(todo => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+    }
   return (
     <div className='TodoWrapper'>
       <h1>Get Things Done</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} toggleComplete={toggleComplete} />
+        <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo} />
       ))}
     </div>
   )
